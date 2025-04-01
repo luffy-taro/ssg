5262c28e7928023d84e7d058d1737bebc9e000e7
Rishikesh <rishikesh@renergytech.com>
2025-04-02 01:09:21 +0530

    add docs dir, remove public

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import os
 import shutil
+import sys
 
 from parser import extract_title, markdown_to_html_node
 
 
 CURRENT_DIRECTORY = os.getcwd()
-PUBLIC_DIR = os.path.join(CURRENT_DIRECTORY, 'public')
+PUBLIC_DIR = os.path.join(CURRENT_DIRECTORY, 'docs')
 STATIC_DIR = os.path.join(CURRENT_DIRECTORY, 'static')
 CONTENT_DIR = os.path.join(CURRENT_DIRECTORY, 'content')
 TEMPLATE_FILE = os.path.join(CURRENT_DIRECTORY, 'template.html')
@@ -22,7 +23,7 @@ def copy_files(src, dest):
             copy_files(item_path, dest_path)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     md_file = open(from_path)
     template_file = open(template_path)
@@ -33,6 +34,10 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md_text)
     html_string = markdown_to_html_node(md_text).to_html()
 
+    if basepath != "/":
+        html_string = html_string.replace('href="', 'href="{BASEPATH}')
+        html_string = html_string.replace('src="', 'src="{BASEPATH}')
+
     template_string = template_string.replace("{{ Title }}", title)
     template_string = template_string.replace("{{ Content }}", html_string)
 
@@ -40,17 +45,17 @@ def generate_page(from_path, template_path, dest_path):
         f.write(template_string)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     listings = os.listdir(dir_path_content)
     for item in listings:
         item_path = os.path.join(dir_path_content, item)
         if os.path.isfile(item_path):
             filename = os.path.splitext(item)[0] + ".html"
-            generate_page(item_path, template_path, os.path.join(dest_dir_path, filename))
+            generate_page(item_path, template_path, os.path.join(dest_dir_path, filename), basepath)
         else:
             dest_path = os.path.join(dest_dir_path, item)
             os.mkdir(dest_path)
-            generate_pages_recursive(item_path, template_path, dest_path)
+            generate_pages_recursive(item_path, template_path, dest_path, basepath)
 
 
 def main():
@@ -68,8 +73,14 @@ def main():
     # copy all static files
     copy_files(STATIC_DIR, PUBLIC_DIR)
 
+
+    # update basepath
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     # convert md files to html and copy to public dir
-    generate_pages_recursive(CONTENT_DIR, TEMPLATE_FILE, PUBLIC_DIR)
+    generate_pages_recursive(CONTENT_DIR, TEMPLATE_FILE, PUBLIC_DIR, basepath)
 
 
 if __name__ == "__main__":
